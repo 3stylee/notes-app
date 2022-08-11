60a42bf69f2f57fea901a300020c4b059fb243e1
Lewis Watt <lewis_watt@Lewiss-Air.broadband>
2022-08-11 22:42:22 +0100

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/login_view.dart';
 import 'package:notes_app/views/register_view.dart';
+import 'package:notes_app/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -34,15 +35,19 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            /*final user = FirebaseAuth.instance.currentUser;
-                if (user?.emailVerified ?? false) {
-                  return const Text('Done');
-                } else {
-                  return const VerifyEmailView();
-                }*/
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
           default:
-            return const Text('loading...');
+            return const CircularProgressIndicator();
         }
       },
     );
